3fe4f4c88243268dffbce983c6303d0b18b5b68a
TamSan27 <se.tamil333@gmail.com>
2021-06-05 22:57:23 +0530

    Retry Analyzer added

--- src/com/vmetry/testng/FirstTestCase.java
+++ src/com/vmetry/testng/FirstTestCase.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class FirstTestCase {
-	
-	@Test
+	//retryAnalyzer used to rerun the falied test cases
+	@Test(retryAnalyzer = RetryAnalyzer.class)
 	public void validateInputData() {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.bing.com/");
+		driver.get("https://www.bing.co/");
 		WebElement srchBox = driver.findElement(By.cssSelector("#sb_form_q"));
 		srchBox.sendKeys("Munish Prabhu Paramanantham");
 	}
