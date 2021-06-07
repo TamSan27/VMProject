0a5e2b30200d718924c26a162342cf02ec025a19
TamSan27 <se.tamil333@gmail.com>
2021-06-07 17:24:15 +0530

    added few codes

---
+++ src/com/interview/pratice/DuplicateCharUsingMap.java
@@ -0,0 +1,14 @@
+package com.interview.pratice;
+
+import java.util.Scanner;
+
+public class DuplicateCharUsingMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the string:");
+    
+	}
+
+}
