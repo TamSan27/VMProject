10ab366affd4bb5e583ef028d32f496d680bfcb9
TamSan27 <se.tamil333@gmail.com>
2021-07-30 18:32:59 +0530

    added web table storage in map

--- src/com/interview/selenium/practice/DynamicWebTable.java
+++ src/com/interview/selenium/practice/DynamicWebTable.java
@@ -1,6 +1,9 @@
 package com.interview.selenium.practice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,10 +13,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DynamicWebTable {
 
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\gowthaman\\git\\VMProject\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		try {
 		// TODO Auto-generated method stub
 		int count = 0;
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tamil\\git\\VMProject\\drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		
 		
 		driver.get("https://www.w3schools.com/html/html_tables.asp");
 		
@@ -31,6 +36,38 @@ public class DynamicWebTable {
 		String countryName = countryEle.getText();
 		System.out.println("Country is:"+countryName);
 		
+		List<WebElement> totalRows = new ArrayList<WebElement>();
+		totalRows = driver.findElements(By.xpath("//table[@id='customers']//tr"));
+		
+		Map<Integer,List<List<String>>> tableValues = new HashMap<Integer,List<List<String>>>();
+		
+		List<WebElement> rowElements;
+		for(int i=1;i<=totalRows.size();i++) {
+			
+			 rowElements = driver.findElements(By.xpath("//table[@id='customers']//tr["+i+"]//*")); 
+			 
+			List<String> rowValues = new ArrayList<String>();
+			for(WebElement e:rowElements) {
+	            rowValues.add(e.getText());
+			}
+			List<List<String>> value = new ArrayList<List<String>>();
+			value.add(rowValues);
+					
+			tableValues.put(i, value);
+				
+				}
+		
+		System.out.println("Printing table values:"+tableValues);
+		
+		for(Map.Entry<Integer, List<List<String>>> en:tableValues.entrySet()) {
+			System.out.println("Row "+en.getKey()+" :"+en.getValue());
+		}
+		
+		
 	}
+		catch(Exception e) {
+			driver.quit();
+		}
 
+}
 }
