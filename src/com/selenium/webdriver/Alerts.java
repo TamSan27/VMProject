2637d31df3900d971275ed5fed19be36de3835f9
TamSan27 <se.tamil333@gmail.com>
2022-01-25 19:29:31 +0530

    push

--- src/com/selenium/webdriver/Alerts.java
+++ src/com/selenium/webdriver/Alerts.java
@@ -13,30 +13,35 @@ public class Alerts {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Tamil\\eclipse-workspace\\SeleniumProject\\SeleniumProject\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
+		driver.get("https://demoqa.com/alerts");
 		Dimension dim = new Dimension(1800, 1200);
 		driver.manage().window().setSize(dim);
-		driver.findElement(By.xpath(getAlertBtn("Simple Alert"))).click();
+		driver.findElement(By.xpath("//button[@id='alertButton']")).click();
 		Thread.sleep(1000);
 		Alert simpleAlert, confirmAlert, prmtAlrt;
 		simpleAlert = driver.switchTo().alert();
 		Thread.sleep(1000);
 		simpleAlert.accept();
-		driver.findElement(By.xpath(getAlertBtn("Confirm Pop up"))).click();
-		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[@id='confirmButton']")).click();
+		Thread.sleep(5000);
 		confirmAlert = driver.switchTo().alert();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		confirmAlert.dismiss();
-		driver.findElement(By.xpath(getAlertBtn("Prompt Pop up"))).click();
-		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[@id='promtButton']")).click();
+		Thread.sleep(4000);
 		prmtAlrt = driver.switchTo().alert();
-		prmtAlrt.sendKeys("Munish");
+		prmtAlrt.sendKeys("values");
 		prmtAlrt.accept();
+		
+		driver.findElement(By.xpath("//button[@id='timerAlertButton']")).click();
+		Alert timeAlrt;
+		timeAlrt = driver.switchTo().alert();
+		timeAlrt.accept();
 	}
 
-	public static String getAlertBtn(String btnName) {
-		return "//button[text()='" + btnName + "']";
-	}
+//	public static String getAlertBtn(String btnName) {
+//		return "//button[text()='" + btnName + "']";
+//	}
 		
 
 }
